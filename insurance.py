66b4425fc445f81129607a14d2288e3cb7633586
Ashutosh Chaurasia <ashutoshc9211@gmail.com>
2026-03-07 13:14:28 +0530

    Create insurance.py

---
+++ insurance.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier, GradientBoost
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import f1_score
+from skleang.metrics import classification_report, confusion_matri
+import warnings
+import pickle
+from scipy import stats
